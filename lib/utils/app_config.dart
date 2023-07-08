52c1b961d4f087a158014854d4ae77999a0ed569
Krins <keyurgajipara1234@gmail.com>
2023-07-08 23:17:12 +0530

    Done

--- lib/utils/app_config.dart
+++ lib/utils/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
   static const String BASE_URL = "https://api.github.com/";
   static const String gitUser = "repos/octocat/Spoon-Knife/issues";
-  static const String token = "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";
+  static const String token = "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";
 }
